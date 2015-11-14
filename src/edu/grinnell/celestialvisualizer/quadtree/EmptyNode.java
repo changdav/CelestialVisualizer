d2503b37ec302b2aca40dc1639dc23ee58c65287
David H. Chang <changdav@grinnell.edu>
2015-11-14 11:44:42 -0600

    Construct leaf and centroid constructors

---
+++ src/edu/grinnell/celestialvisualizer/quadtree/EmptyNode.java
@@ -0,0 +1,30 @@
+package edu.grinnell.celestialvisualizer.quadtree;
+import edu.grinnell.celestialvisualizer.util.BoundingBox;
+import edu.grinnell.celestialvisualizer.util.Point;
+import edu.grinnell.celestialvisualizer.util.Vector2d;
+
+public class EmptyNode implements Node{
+	
+	
+	//  Constructor
+	//	public EmptyNode {
+	//		
+	//	}
+	
+	@Override
+	public boolean lookup(Point pos, BoundingBox bb) {
+		return false;
+	}
+
+	@Override
+	public Vector2d calculateAcceleration(Point p, BoundingBox bb, double thresh) {
+		return Vector2d.zero;
+	}
+
+	@Override
+	public Node insert(double mass, Point p, BoundingBox bb) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
