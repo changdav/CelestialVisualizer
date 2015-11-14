e238ce8b69dfaee71656b16500033a4fe4b2617f
David H. Chang <changdav@grinnell.edu>
2015-11-14 12:40:58 -0600

    Update LookupTest and Node constructors

--- src/edu/grinnell/celestialvisualizer/quadtree/EmptyNode.java
+++ src/edu/grinnell/celestialvisualizer/quadtree/EmptyNode.java
@@ -4,18 +4,21 @@ import edu.grinnell.celestialvisualizer.util.Point;
 import edu.grinnell.celestialvisualizer.util.Vector2d;
 
 public class EmptyNode implements Node{
+	// does this have fields???
 	
+	public EmptyNode() {}
 	
-	//  Constructor
-	//	public EmptyNode {
-	//		
-	//	}
-	
+	/**
+	 * This method returns false as an EmptyNode does not contain anything.
+	 */
 	@Override
 	public boolean lookup(Point pos, BoundingBox bb) {
 		return false;
 	}
 
+	/**
+	 * This method returns a Vector2d of (0,0) as an EmptyNode exerts no force.
+	 */
 	@Override
 	public Vector2d calculateAcceleration(Point p, BoundingBox bb, double thresh) {
 		return Vector2d.zero;
--- src/edu/grinnell/celestialvisualizer/quadtree/QuadTree.java
+++ src/edu/grinnell/celestialvisualizer/quadtree/QuadTree.java
@@ -5,41 +5,54 @@ import edu.grinnell.celestialvisualizer.util.Point;
 import edu.grinnell.celestialvisualizer.util.Vector2d;
 
 public class QuadTree {
-    
-    public boolean lookup(Point pos, BoundingBox bb) {
-        throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.lookup");
-    }
-    
-    public Vector2d calculateAcceleration(Point p, BoundingBox bb, double eps) {
-        throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.calculateAcceleration");
-    }
-    
-    public void insert(double mass, Point pos, BoundingBox bb) {
-        throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.insert");
-    }
-    
-    @Override
-    public boolean equals(Object other) {
-        throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.equals");
-    }
-    
-    public static QuadTree q0() {
-        throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.q0");
-    }
-    
-    public static QuadTree q1() {
-        throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.q1");
-    }
-    
-    public static QuadTree q2() {
-        throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.q2");
-    }
-    
-    public static QuadTree q3() {
-        throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.q3");
-    }
-    
-    public static final QuadTree q4() {
-        throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.q4");
-    }
+	private Node root;
+
+	public QuadTree() {}
+
+	public static QuadTree makeTreeWithLeaf() {
+		QuadTree ret = new QuadTree();
+		ret.root = new LeafNode(1.0, new Point (2.0, 3.0));
+		return ret;
+	}
+
+	public Node getRoot() { 
+		return root;
+	}
+
+	public boolean lookup(Point pos, BoundingBox bb) {
+		throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.lookup");
+	}
+
+	public Vector2d calculateAcceleration(Point p, BoundingBox bb, double eps) {
+		throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.calculateAcceleration");
+	}
+
+	public void insert(double mass, Point pos, BoundingBox bb) {
+		throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.insert");
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.equals");
+	}
+
+	public static QuadTree q0() {
+		throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.q0");
+	}
+
+	public static QuadTree q1() {
+		throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.q1");
+	}
+
+	public static QuadTree q2() {
+		throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.q2");
+	}
+
+	public static QuadTree q3() {
+		throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.q3");
+	}
+
+	public static final QuadTree q4() {
+		throw new edu.grinnell.celestialvisualizer.UnimplementedException("QuadTree.q4");
+	}
 }
