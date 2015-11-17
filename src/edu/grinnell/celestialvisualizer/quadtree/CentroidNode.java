ce9cf60be62c782262f83bd79265f1d3b524d811
Niko Takayesu <takayesu17@cs.grinnell.edu>
2015-11-17 11:43:18 -0600

    updated LeafNode insert

--- src/edu/grinnell/celestialvisualizer/quadtree/CentroidNode.java
+++ src/edu/grinnell/celestialvisualizer/quadtree/CentroidNode.java
@@ -1,23 +1,24 @@
 package edu.grinnell.celestialvisualizer.quadtree;
 import edu.grinnell.celestialvisualizer.util.Quadrant;
 import edu.grinnell.celestialvisualizer.physics.Centroid;
+import edu.grinnell.celestialvisualizer.physics.Physics;
 import edu.grinnell.celestialvisualizer.util.BoundingBox;
 import edu.grinnell.celestialvisualizer.util.Point;
 import edu.grinnell.celestialvisualizer.util.Vector2d;
 
 public class CentroidNode implements Node{
 	private Centroid cent;
-	private Node q0;
-	private Node q1;
-	private Node q2;
-	private Node q3;
+	private Node upperLeft;
+	private Node upperRight;
+	private Node lowerLeft;
+	private Node lowerRight;
 
-	public CentroidNode (Centroid cent, Node q0, Node q1, Node q2, Node q3){
+	public CentroidNode (Centroid cent, Node upperLeft, Node upperRight, Node lowerLeft, Node lowerRight){
 		this.cent = cent;
-		this.q0 = q0;
-		this.q1 = q1;
-		this.q2 = q2;
-		this.q3 = q3;
+		this.upperLeft = upperLeft;
+		this.upperRight = upperRight;
+		this.lowerLeft = lowerLeft;
+		this.lowerRight = lowerRight;
 	}
 
 	//done
@@ -35,13 +36,25 @@ public class CentroidNode implements Node{
 	//done
 	@Override
 	public Vector2d calculateAcceleration(Point p, BoundingBox bb, double thresh) {
-		if 
+		double distance = p.distance(this.cent.getPosition()).magnitude();
+		if (this.lookup(p, bb) || distance < thresh) {
+			return calculateAcceleration(p, bb.getQuadrant(Quadrant.UPPER_LEFT), thresh)
+					.add(calculateAcceleration(p, bb.getQuadrant(Quadrant.UPPER_RIGHT), thresh))
+							.add(calculateAcceleration(p, bb.getQuadrant(Quadrant.LOWER_LEFT), thresh))
+									.add(calculateAcceleration(p, bb.getQuadrant(Quadrant.LOWER_RIGHT), thresh));
+		} else {
+			return Physics.calculateAccelerationOn(p, cent.getMass(), cent.getPosition());
+		}
+	}
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof CentroidNode) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-    public boolean equals(Object other) {
-    	
-    }
-	
 	@Override
 	public Node insert(double mass, Point p, BoundingBox bb) {
 		// TODO Auto-generated method stub
