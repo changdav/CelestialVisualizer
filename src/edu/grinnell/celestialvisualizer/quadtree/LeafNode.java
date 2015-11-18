442153282f31de12108235808edd2446c6dee7d6
changdav <changdav@grinnell.edu>
2015-11-17 19:31:50 -0600

    leafNode.insert done?

--- src/edu/grinnell/celestialvisualizer/quadtree/LeafNode.java
+++ src/edu/grinnell/celestialvisualizer/quadtree/LeafNode.java
@@ -38,70 +38,20 @@ public class LeafNode implements Node{
 		}
 	}
 
-	//	public Node determineQuad(double mass, Point p, BoundingBox bb) {
-	//		if (bb.quadrantOf(position) == Quadrant.UPPER_LEFT && bb.quadrantOf(p) == Quadrant.UPPER_RIGHT) {
-	//			Centroid b1 = new Centroid(this.mass, this.position);
-	//			Centroid b2 = new Centroid(mass, p);
-	//			return new CentroidNode(b1.add(b2),
-	//					new LeafNode(this.mass, this.position),
-	//					new )
-	//		}
-	//		
-	//		return new 
-	//	}
-
 	@Override
 	public Node insert(double mass, Point p, BoundingBox bb) {
 		if (!bb.contains(p)){
 			throw new UnsupportedOperationException();
-		} else if (p.getX() == this.position.getX() && p.getY() == this.position.getY()){
+		} else if (p.equals(position)){
 			return new LeafNode (this.mass + mass, p);
 		} else {
-			return new CentroidNode()
-		}
-
-
-
-
-		if (bb.quadrantOf(p).equals(bb.quadrantOf(position))){
-			return insert (mass, p, bb.getQuadrant(bb.quadrantOf(p)));
-		} else {
-			Centroid cent1 = new Centroid (this.mass, this.position);
-			Centroid cent2 = new Centroid (mass, p);
-
-			Node upperLeft = new EmptyNode();
-			Node upperRight= new EmptyNode();
-			Node lowerLeft = new EmptyNode();
-			Node lowerRight= new EmptyNode();
-
-			if (bb.getQuadrant(bb.quadrantOf(p)) == bb.getQuadrant(Quadrant.UPPER_LEFT)){
-				LeafNode ret = new LeafNode (mass, p);
-				upperLeft = ret;
-			} else if (bb.getQuadrant(bb.quadrantOf(position)) == bb.getQuadrant(Quadrant.UPPER_LEFT)){
-				LeafNode ret = new LeafNode (this.mass, position);
-				upperLeft = ret;
-			} else if (bb.getQuadrant(bb.quadrantOf(p)) == bb.getQuadrant(Quadrant.UPPER_RIGHT)){
-				LeafNode ret = new LeafNode (mass, p);
-				upperRight = ret;
-			} else if (bb.getQuadrant(bb.quadrantOf(position)) == bb.getQuadrant(Quadrant.UPPER_RIGHT)){
-				LeafNode ret = new LeafNode (this.mass, position);
-				upperRight= ret;
-			} else if (bb.getQuadrant(bb.quadrantOf(p)) == bb.getQuadrant(Quadrant.LOWER_LEFT)){
-				LeafNode ret = new LeafNode (mass, p);
-				lowerLeft = ret;
-			} else if (bb.getQuadrant(bb.quadrantOf(position)) == bb.getQuadrant(Quadrant.LOWER_LEFT)){
-				LeafNode ret = new LeafNode (this.mass, position);
-				lowerLeft = ret;
-			} else if (bb.getQuadrant(bb.quadrantOf(p)) == bb.getQuadrant(Quadrant.LOWER_RIGHT)){
-				LeafNode ret = new LeafNode (mass, p);
-				lowerRight = ret;
-			} else if (bb.getQuadrant(bb.quadrantOf(position)) == bb.getQuadrant(Quadrant.LOWER_RIGHT)){
-				LeafNode ret = new LeafNode (this.mass, position);
-				lowerRight = ret;
-			}
-
-			return new CentroidNode(cent2.add(cent1), upperLeft, upperRight, lowerLeft, lowerRight);
+			Centroid b1 = new Centroid(this.mass, this.position);
+			Centroid b2 = new Centroid(mass, p);
+			Centroid b3 = b1.add(b2);
+			CentroidNode cNode = new CentroidNode(b3, new EmptyNode(), new EmptyNode(), new EmptyNode(), new EmptyNode());
+			cNode.insert(mass, p, bb);
+			cNode.insert(this.mass, position, bb);
+			return cNode;
 		}
 	}
-
 }
